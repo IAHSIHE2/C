dfc89cb604e102d039c11defe96a989c84be2d3b
IAHSIHE2 <chaymaenaji20022@gmail.com>
2024-10-05 19:05:21 +0100

    Update r.py

--- r.py
+++ r.py
@@ -19,3 +19,17 @@ def save(message):
 
 ###################################################################################
 print(save("h"))
+
+
+
+from urllib.request import urlopen
+import re as r
+
+def getIP():
+    d = str(urlopen('http://checkip.dyndns.com/')
+            .read())
+
+    return r.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').
+             search(d).group(1)
+
+print(getIP())
